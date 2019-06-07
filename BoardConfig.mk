78fb9086618cb55cd97b45747dbb168288658321
Felix <google@ix5.org>
2019-06-07 09:23:00 +0200

    begonia: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated and references should be updated.
    
    Signed-off-by: Felix <google@ix5.org>
    Change-Id: I063940a63256a881206740e8a7ecae215f3a5ca8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ include device/mediatek/sepolicy/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/begonia/BoardConfigVendor.mk
