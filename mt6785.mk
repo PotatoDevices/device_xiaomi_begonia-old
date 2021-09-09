3f69addfc072758e2d6c394e09cba74c53f542a1
TheMalachite <eliasgheeraert@gmail.com>
2021-09-09 23:58:49 +0200

    mt6785-common: Build DRM 1.0 lazy service
    - Apps like Chrome want it for avoid to crash even tho it's not present on stock vendor
    
    Change-Id: I1090a6000644a0573314115518a1489cb673ea6b

--- mt6785.mk
+++ mt6785.mk
@@ -114,6 +114,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
 # Gatekeeper
