49248c554f5557db42535475f898953c4039e73c
Vaisakh Murali <mvaisakh@statixos.com>
2021-10-18 18:49:05 +0600

    begonia: Link libmtkcam_stdutils against VNDK 30 libutils
    libmtkcam_stdutils is an Android 11 MTK proprietary blob which depends
    upon libutils for certain symbols. The newer Android 12 platform
    libutils does not provide these.
    Link it against prebuilt VNDK 30 libutils.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I02bbcf9e6df2df7ca7c03ffc7c1dd7eeb5ffe222
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/5876
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -58,8 +58,14 @@ function blob_fixup {
         lib/libsink.so)
             "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
             ;;
+        vendor/lib/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
         vendor/lib/libudf.so)
             "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "${2}"
+            ;;
+        vendor/lib64/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
         vendor/lib64/libudf.so)
             "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "${2}"
