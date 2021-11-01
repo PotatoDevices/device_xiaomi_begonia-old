ca1d83fc47bff12eb6f0fc3092f2f47e3d12f5c4
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-01 09:56:08 +0100

    begonia: Switch to OSS audio HAL service
    Prebuilt audio HAL depends on a lot of VNDK 30 dependencies, using
    it as is requires a lot of hacks.
    Switching to OSS HAL does not require hexediting any prebuilt blobs
    and their depdendencies.
    
    Change-Id: Ib31b440e33dabde8f8971d489cf27463ad61030c
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/5947
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl
