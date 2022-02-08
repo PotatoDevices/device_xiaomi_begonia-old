644a870ee17205b6bb0a3491352ab10257db1a60
SahilSonar <sss.sonar2003@gmail.com>
2022-02-08 12:28:16 +0100

    begonia: Update build fingerprint to SQ1A.220205.002

--- potato_begonia.mk
+++ potato_begonia.mk
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.5.0.RGGMIXM release-keys" \
