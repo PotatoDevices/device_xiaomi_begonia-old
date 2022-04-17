adbee6fea473e3c16879909da9a899815b25611b
Vaisakh Murali <mvaisakh@statixos.com>
2022-04-17 21:58:51 +0600

    begonia: dt2w: Refactor double tap service
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- dt2w/src/org/lineageos/dt2w/begonia/DoubleTapService.kt
+++ dt2w/src/org/lineageos/dt2w/begonia/DoubleTapService.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2022 StatiX
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
 package org.lineageos.dt2w.begonia
 
 import android.app.Service
@@ -13,10 +18,10 @@ import android.os.SystemProperties
 import android.os.UserHandle
 import android.provider.Settings.Secure
 
-class DT2WServiceBegonia : Service() {
-    private var mContext: Context
-    private var mHandler: Handler
-    private var mCustomSettingsObserver: CustomSettingsObserver
+class DoubleTapService : Service() {
+    private lateinit var mContext: Context
+    private lateinit var mHandler: Handler
+    private lateinit var mCustomSettingsObserver: CustomSettingsObserver
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -53,9 +58,5 @@ class DT2WServiceBegonia : Service() {
                 update()
             }
         }
-    }
-
-    companion object {
-        private const val TAG = "DT2WServiceBegonia"
     }
 }
--- dt2w/src/org/lineageos/dt2w/begonia/OnBootCompleteReceiver.kt
+++ dt2w/src/org/lineageos/dt2w/begonia/OnBootCompleteReceiver.kt
@@ -1,12 +1,22 @@
+/*
+ * Copyright (C) 2022 StatiX
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
 package org.lineageos.dt2w.begonia
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
-class OnBootCompleteReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
-        val sIntent = Intent(context, DT2WServiceBegonia::class.java)
-        context.startServiceAsUser(sIntent, UserHandle.CURRENT)
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Starting")
+        DoubleTapUtils.startService(context)
+    }
+
+    companion object {
+        private const val TAG = "DT2WServiceBegonia"
     }
 } 
