529cc187d69581f8072e9ecfeab6b3b966eee81f
SahilSonar <sss.sonar2003@gmail.com>
2022-06-22 18:41:43 +0200

    begonia: Drop dynamic retrofit

--- device.mk
+++ device.mk
@@ -262,13 +262,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
     $(DEVICE_PATH)/nfc/libnfc-nxp-pnscr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-pnscr.conf
 
-# Partitions
-PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
-
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
--- releasetools.py
+++ releasetools.py
@@ -16,42 +16,6 @@
 
 import common
 import re
-import os
-from common import BlockDifference, EmptyImage, GetUserImage
-
-# The joined list of user image partitions of source and target builds.
-# - Items should be added to the list if new dynamic partitions are added.
-# - Items should not be removed from the list even if dynamic partitions are
-#   deleted. When generating an incremental OTA package, this script needs to
-#   know that an image is present in source build but not in target build.
-USERIMAGE_PARTITIONS = [
-    "odm",
-    "product",
-    "system_ext",
-]
-
-def GetUserImages(input_tmp, input_zip):
-  return {partition: GetUserImage(partition, input_tmp, input_zip)
-          for partition in USERIMAGE_PARTITIONS
-          if os.path.exists(os.path.join(input_tmp,
-                                         "IMAGES", partition + ".img"))}
-
-def FullOTA_GetBlockDifferences(info):
-  images = GetUserImages(info.input_tmp, info.input_zip)
-  return [BlockDifference(partition, image)
-          for partition, image in images.items()]
-
-def IncrementalOTA_GetBlockDifferences(info):
-  source_images = GetUserImages(info.source_tmp, info.source_zip)
-  target_images = GetUserImages(info.target_tmp, info.target_zip)
-
-  # Use EmptyImage() as a placeholder for partitions that will be deleted.
-  for partition in source_images:
-    target_images.setdefault(partition, EmptyImage())
-
-  # Use source_images.get() because new partitions are not in source_images.
-  return [BlockDifference(partition, target_image, source_images.get(partition))
-          for partition, target_image in target_images.items()]
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
@@ -61,8 +25,8 @@ def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def AddImage(info, dir, basename, dest):
-  path = dir + "/" + basename
+def AddImage(info, basename, dest):
+  path = "IMAGES/" + basename
   if path not in info.input_zip.namelist():
     return
 
@@ -70,15 +34,8 @@ def AddImage(info, dir, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def FullOTA_InstallBegin(info):
-  AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
-  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
-  info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
-  info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
-  return
-
 def OTA_InstallEnd(info):
   info.script.Print("Patching device-tree and vbmeta images...")
-  AddImage(info, "IMAGES", "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo")
-  AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
+  AddImage(info, "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
   return
